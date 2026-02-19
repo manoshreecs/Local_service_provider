a2b942e686fbd6c0dfa7d552b306ae5ef0ac1362
manoshreecs <cmanoshree@gmail.com>
2026-02-19 17:13:39 +0530

    fixed backend api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,13 +2,14 @@ import axios from "axios";
 
 const API_BASE_URL =
     process.env.NODE_ENV === "production" ?
-    "https://local-service-provider-1-f3vc.onrender.com/api" :
+    "https://backend-service-fyex.onrender.com/api" :
     "http://localhost:5000/api";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
     withCredentials: false,
 });
+
 api.interceptors.request.use((req) => {
     const token = localStorage.getItem("token");
     if (token) {
