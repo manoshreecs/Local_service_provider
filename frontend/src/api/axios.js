4521113d659cc3f2609a1e827e965b32ea41ab2f
manoshreecs <cmanoshree@gmail.com>
2026-02-21 12:27:38 +0530

    fix: updated axios baseURL for production using import.meta.env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,21 @@
 import axios from "axios";
 
+const API_BASE_URL =
+    import.meta.env.MODE === "production" ?
+    "https://backend-service-fyex.onrender.com/api" :
+    "http://localhost:5000/api";
+
 const api = axios.create({
-    baseURL: "https://backend-service-fyex.onrender.com/api",
-    withCredentials: true
+    baseURL: API_BASE_URL,
 });
 
 api.interceptors.request.use((req) => {
     const token = localStorage.getItem("token");
+
     if (token) {
         req.headers.Authorization = `Bearer ${token}`;
     }
+
     return req;
 });
 
